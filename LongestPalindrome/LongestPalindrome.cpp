8beef47b43beb629cdee21d64c5f74bcd599b8ea
Rohit Jairaj Singh <rohit@singh.org.in>
2024-07-11 17:13:01 +0000

    Added LongestPalindrome.cpp

---
+++ LongestPalindrome/LongestPalindrome.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+
+auto LongestPallindromeSubstring(const std::string &text) {
+    size_t max_size = 1;
+    size_t max_center = 0;
+
+    std::vector<size_t> track(text.size(), 1 );
+
+    size_t current_center { 1 };
+    auto left {current_center - 1};
+    auto right {current_center + 1};
+    while(true) {
+
+        while(left != std::numeric_limits<size_t>::max() && right < text.size()) {
+            if (text[left] != text[right]) break;
+            --left; ++right;
+        }
+
+        auto current_size {right - left - 1};
+        track[current_center] = current_size;
+        if (current_size > max_size) {
+            max_center = current_center;
+            max_size = current_size;
+        }
+
+        auto new_center { current_center + 1 };
+        auto previous_size_loc { current_center - 1};
+        auto previous_size { track[previous_size_loc] };
+        while((previous_size - 1)/2 + new_center < right - 1) {
+            --previous_size_loc;
+            ++new_center;
+            previous_size = track[previous_size_loc];
+        }
+
+        if (new_center >= text.size()) break;
+
+        current_center = new_center;
+        left = new_center * 2 - right;
+    }
+
+
+    return std::pair { max_center, max_size };
+}
+
+
+int main(int argc, char *argv[]) {
+    // This works only for odd palindrom
+    std::vector<std::string> values {
+        "abaxabxbaxabb"
+    };
+
+    for(auto value: values) {
+        auto [center, size] = LongestPallindromeSubstring(value);
+        auto start = center - (size - 1) / 2;
+        auto end = center + (size - 1) / 2;
+        auto substr = value.substr(start, end);
+        std::cout << "Original: " << value << ",  " << substr << std::endl;
+    }
+
+    return 0;
+}
