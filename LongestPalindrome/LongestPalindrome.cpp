0d5fd8b1d8158fc0d567b24bc60f6618d895c473
Rohit Jairaj Singh <rohit@singh.org.in>
2024-07-12 16:34:31 +0000

    Support for even palindrome.

--- LongestPalindrome/LongestPalindrome.cpp
+++ LongestPalindrome/LongestPalindrome.cpp
@@ -6,11 +6,12 @@ auto LongestPallindromeSubstring(const std::string &text) {
     size_t max_size = 1;
     size_t max_center = 0;
 
-    std::vector<size_t> track(text.size(), 1 );
+    std::vector<size_t> track(text.size() * 2 - 1, 0 );
+    track[0] = 1;
 
     size_t current_center { 1 };
     auto left {current_center - 1};
-    auto right {current_center + 1};
+    auto right {current_center};
     while(true) {
 
         while(left != std::numeric_limits<size_t>::max() && right < text.size()) {
@@ -19,27 +20,28 @@ auto LongestPallindromeSubstring(const std::string &text) {
         }
 
         auto current_size {right - left - 1};
-        track[current_center] = current_size;
+        auto track_index { (current_center * 2) - !(current_size % 2) };
+        track[track_index] = current_size;
         if (current_size > max_size) {
-            max_center = current_center;
+            max_center = current_center - !(current_size % 2);
             max_size = current_size;
         }
 
-        auto new_center { current_center + 1 };
-        auto previous_size_loc { current_center - 1};
+        auto new_track_index { track_index + 1 };
+        auto previous_size_loc { track_index - 1};
         auto previous_size { track[previous_size_loc] };
-        while((previous_size - 1)/2 + new_center < right - 1) {
+        while((previous_size - 1 + new_track_index)/2 < right - 1) {
+            track[new_track_index] = track[previous_size_loc];
             --previous_size_loc;
-            ++new_center;
+            ++new_track_index;
             previous_size = track[previous_size_loc];
         }
 
-        if (new_center >= text.size()) break;
+        if ((new_track_index + 1)/2 >= text.size()) break;
 
-        current_center = new_center;
-        left = new_center * 2 - right;
+        current_center = (new_track_index + 1)/2;
+        left = new_track_index - right;
     }
-
 
     return std::pair { max_center, max_size };
 }
@@ -48,6 +50,7 @@ auto LongestPallindromeSubstring(const std::string &text) {
 int main(int argc, char *argv[]) {
     // This works only for odd palindrom
     std::vector<std::string> values {
+        "bb",
         "abaxabxbaxabb",
         "a",
         "aa",
