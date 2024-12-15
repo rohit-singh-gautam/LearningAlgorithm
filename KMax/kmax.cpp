ea72a8e7e9a611df34fd32b9bef4a7d315fb3b4b
Rohit Jairaj Singh <rohit@singh.org.in>
2024-12-15 19:10:43 +0530

    KMax code added.

---
+++ KMax/kmax.cpp
@@ -0,0 +1,64 @@
+#include "kmax.h"
+#include <iostream>
+
+template <typename NumType>
+std::ostream &operator<<(std::ostream &os, const std::vector<NumType> &values) {
+    os << "{ ";
+    for(auto &value: values) {
+        os << value << ' ';
+    }
+    os << '}';
+    return os;
+}
+
+template <typename NumType>
+std::ostream &operator<<(std::ostream &os, const std::pair<std::vector<NumType>, size_t> &value) {
+    os << value.first << '[' << value.second << ']';
+    return os;
+}
+
+template <typename comparator, typename NumType> 
+bool CheckResult(std::vector<NumType> &values, std::vector<NumType> &result) {
+    std::sort(std::begin(values), std::end(values), comparator {});
+    std::sort(std::begin(result), std::end(result), comparator {});
+
+    for(size_t index { 0 }; index < result.size(); ++index) {
+        if (values[index] != result[index]) return false;
+    }
+    return true;
+}
+
+template <typename comparator, typename NumType>
+void test(std::vector<std::pair<std::vector<NumType>, size_t>> &tests) {
+    for(auto &test: tests) {
+        std::cout << test << ": ";
+        auto result = GetTopK<comparator>(test.first, test.second);
+        std::cout << result << " - ";
+        auto testresult = CheckResult<comparator>(test.first, result);
+        if (testresult) std::cout << "Pass";
+        else std::cout << "Fail";
+        std::cout << '\n';
+    }
+}
+
+int main(int const argc, char const *argv[]) {
+    std::vector<std::pair<std::vector<int>, size_t>> tests {
+        {{35, 12, 24, 55, 10}, 3},
+        {{10}, 1},
+        {{10, 20}, 2},
+        {{10, 20}, 1},
+        {{20, 10}, 1},
+        {{30, 40, 10, 20, 90, 50, 11, 24, 55, 66, 77, 39, 24, 54}, 5},
+        {{30, 40, 10, 20, 90, 50, 11, 24, 55, 66, 77, 39, 24, 54}, 2},
+        {{30, 40, 10, 20, 90, 50, 11, 24, 55, 66, 77, 39, 24, 54}, 10},
+        {{30, 40, 10, 20, 90, 50, 11, 24, 55, 66, 77, 39, 24, 54}, 1},
+        {{30, 40, 10, 20, 90, 50, 11, 24, 55, 66, 77, 39, 24, 54}, 5},
+        {{30, 40, 10, 20, 90, 50, 11, 24, 55, 66, 77, 39, 24, 54}, 4},
+        {{30, 40, 10, 20, 90, 50, 11, 24, 55, 66, 77, 39, 24, 54}, 8},
+    };
+
+    test<std::less<>>(tests);
+    test<std::greater<>>(tests);
+
+    return 0;
+}
---
+++ KMax/kmax.h
@@ -0,0 +1,33 @@
+#include <vector>
+
+template <typename comparator, typename NumType>
+size_t CreatePartition(std::vector<NumType> &list, size_t list_start, size_t list_end) {
+    const NumType pivot { list[list_start] };
+    size_t le_index { list_start };
+    size_t g_index { list_start + 1 };
+    for(;g_index < list.size();++g_index) {
+        if (comparator {} (list[g_index], pivot)) {
+            std::swap(list[++le_index], list[g_index]);
+        }
+    }
+    std::swap(list[list_start], list[le_index]);
+    return le_index;
+}
+
+template <typename comparator, typename NumType>
+std::vector<NumType> GetTopK(std::vector<NumType> &list, size_t k) {
+    if (list.size() <= k) return list;
+    if (k == 0) return std::vector<NumType> { };
+    size_t list_start { 0 };
+    size_t list_end = list.size() - 1;
+    for(;;) {
+        auto center = CreatePartition<comparator>(list, list_start, list_end);
+        if (center == k - 1) break;
+        if (center < k - 1) list_start = center + 1;
+        if (center >= k) list_end = center - 1;
+        if (list_start == list_end) break;
+    }
+
+    std::vector<NumType> ret { std::begin(list), std::begin(list) + k };
+    return ret;
+}
